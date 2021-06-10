825cd164b386ab0d52df8260027d3c3ec01ff15f
s173953 <s173953@studdent.dtu.dk>
2021-06-10 15:07:49 +0200

    Tensorboard not working

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,25 +4,23 @@ import os
 
 import torch
 from torch import nn, optim
+from torch.utils.tensorboard import SummaryWriter
 import torchvision
 from torchvision import datasets, transforms
 #import torch.nn.functional as F
 import pathlib
 os.chdir(pathlib.Path().absolute())
 
-
-
-
 from model import NeuralNetwork
 
 import numpy as np
 import pathlib
 import matplotlib.pyplot as plt
-#import plotext.plot as plx
 
-MODEL_PATH = pathlib.Path().absolute()
-ROOT_PATH =  str(pathlib.Path(*MODEL_PATH.parts[:-2]))
-MODEL_PATH = str(MODEL_PATH)
+writer = SummaryWriter()
+
+ROOT_PATH =  str(pathlib.Path(*pathlib.Path().absolute().parts[:-2]))
+MODEL_PATH = ROOT_PATH + "/src/models"
 DATA_PATH = ROOT_PATH + "/data/processed"
 
 
@@ -48,6 +46,7 @@ class Train(object):
         epochs = 3#20
         steps = 0
         train_losses = []
+        train_accuracy = 0
         plot_loss = []
         for e in range(epochs):
             running_loss = 0
@@ -59,13 +58,26 @@ class Train(object):
                 optimizer.step()
                 running_loss += loss.item()
                 steps += 1
+
+                train_ps = torch.exp(model(images))
+                train_top_p, train_top_class = train_ps.topk(1, dim=1)
+                train_equals = train_top_class == labels.view(*train_top_class.shape)
+
                 train_losses.append(loss.item()/64)
+                train_accuracy += torch.mean(train_equals.type(torch.FloatTensor))
+
+                writer.add_scalar('Loss/train', train_losses[-1], steps)
+                writer.add_scalar('Accuracy/train', train_accuracy/steps, steps)
+
             plot_loss.append(running_loss/len(train_set))
             print(f"Training loss: {running_loss/len(train_set)}")
 
         plt.plot(np.arange(epochs),plot_loss)
         plt.savefig(ROOT_PATH+'/reports/figures/loss.png')
+        save_results_to = ROOT_PATH+'/reports/figures/'
         torch.save(model.state_dict(), MODEL_PATH+'/trained_models/model_v1.pth')
+    
+
 
 if __name__ == '__main__':
     Train()
