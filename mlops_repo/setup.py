1e563577781e93f8a3943f30ad913b89311d5211
s173953 <s173953@studdent.dtu.dk>
2021-06-10 09:33:43 +0200

    Cookiecutter initiated

---
+++ mlops_repo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project in Machine Learning Operations at DTU',
+    author='lbh',
+    license='MIT',
+)
